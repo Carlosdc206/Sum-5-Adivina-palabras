554d32cee2af3a00b685842627f1401fcdfe2f42
Carlosdc206 <89317743+Carlosdc206@users.noreply.github.com>
2021-08-21 16:33:28 -0500

    Add files via upload

---
+++ Sum #5.cpp
@@ -0,0 +1,35 @@
+#define "silvestre"
+#define carro 10
+
+int main(){
+	char palabra [silvestre + 1];
+	int intento = 0;
+	int ingresa = 0; //
+	do {
+	system("cls");
+	printf("\n\t\t\t escriba la palabra\n");
+	printf("\t\t\t------------\n");
+	printf("\n\t palabra: sapo");
+	gets(palabra);
+	
+	if (strcmp(palabra, Palabra) == 0 ){
+		ingresa = 1;
+	}else{
+		printf("\n\t palabra incorrecta\n");
+		intento++;
+		getchar();
+	}
+	
+	
+	}while (intentos < 2 && ingresa == 0);
+	
+	
+	if (ingresa == 1){
+		printf("\n\t dinero\n");
+	
+	}else}
+		printf("\n\termino el numero de intentos\n");
+	}
+	return 0;
+	}
+
